27ef7049a9d91eea63b5de8f5f590ed399ec7912
Claudio D. Sanchez <lmhj.luijo@gmail.com>
2023-02-26 11:48:02 -0400

    feat: make obstacles solid

--- script.js
+++ script.js
@@ -53,8 +53,13 @@ window.addEventListener('load', function() {
             this.collisionY += this.speedY * this.speedModifier;
 
             this.game.obstacles.forEach(obstacle => {
-                if(this.game.checkCollision(this, obstacle)) {
-                    console.log('collision')
+                // ? [(distance < sumOfRadii), distance, sumOfRadii, dx, dy]
+                const [collision, distance, sumOfRadii, dx, dy] = this.game.checkCollision(this, obstacle)
+                if(collision) {
+                    const unit_x = dx / distance;
+                    const unit_y = dy / distance;
+                    this.collisionX = obstacle.collisionX + (sumOfRadii + 1) * unit_x;
+                    this.collisionY = obstacle.collisionY + (sumOfRadii + 1) * unit_y;
                 }
             })
         }
@@ -135,7 +140,7 @@ window.addEventListener('load', function() {
             const dy = a.collisionY - b.collisionY;
             const distance = Math.hypot(dy, dx);
             const sumOfRadii = a.collisionRadius + b.collisionRadius;
-            return (distance < sumOfRadii);
+            return [(distance < sumOfRadii), distance, sumOfRadii, dx, dy];
         }
 
         init() {
