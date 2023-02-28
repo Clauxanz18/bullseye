10145fdccb819a5416604ce405995ce7af646316
Claudio D. Sanchez <lmhj.luijo@gmail.com>
2023-02-28 19:28:05 -0400

    feat: add player boundaries

--- script.js
+++ script.js
@@ -81,6 +81,18 @@ window.addEventListener('load', function() {
             this.spriteX = this.collisionX - this.width * 0.5;
             this.spriteY = this.collisionY - this.height * 0.5 - 100;
 
+            // * horizontal Boundaries
+            if(this.collisionX < this.collisionRadius)
+            this.collisionX = this.collisionRadius;
+            else if (this.collisionX > this.game.width - this.collisionRadius)
+            this.collisionX = this.game.width - this.collisionRadius;
+
+            // * Vertical Boundaries
+            if(this.collisionY < this.game.topMargin + this.collisionRadius)
+            this.collisionY = this.game.topMargin + this.collisionRadius;
+            else if (this.collisionY > this.game.height - this.collisionRadius)
+            this.collisionY = this.game.height - this.collisionRadius;
+
             this.game.obstacles.forEach(obstacle => {
                 // ? [(distance < sumOfRadii), distance, sumOfRadii, dx, dy]
                 const [collision, distance, sumOfRadii, dx, dy] = this.game.checkCollision(this, obstacle)
@@ -197,7 +209,7 @@ window.addEventListener('load', function() {
                     }
                 })
                 
-                const margin = testObstacle.collisionRadius * 2;
+                const margin = testObstacle.collisionRadius * 3;
                 const isContainedX = (testObstacle.spriteX > 0) && (testObstacle.spriteX < this.width - testObstacle.width);
                 const isContainedY = (testObstacle.collisionY > this.topMargin + margin) && (testObstacle.collisionY < this.height - margin);
 
